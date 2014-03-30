8930d1b83fee547855ee2ac9f93091d71c294edd
masa <masa.shimomatsu@gmail.com>
2014-03-30 13:23:18 +0900

    Modify activity

--- bonsai/src/aruite/tanoshiku/bousaikunren/MainActivity.java
+++ bonsai/src/aruite/tanoshiku/bousaikunren/MainActivity.java
@@ -4,33 +4,15 @@ import com.google.android.gms.maps.GoogleMap;
 
 
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-import android.os.Build;
-
-import com.google.android.gms.maps.MapFragment;
 
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends SingleFragmentActivity {
-
-	@Override
-	protected Fragment createFragment() {
-	
-		return MapFragment.newInstance();
-		
-	}
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
-
