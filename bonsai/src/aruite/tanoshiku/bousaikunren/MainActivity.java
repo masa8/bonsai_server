57392773bca4364c15423b363f57ac6e30f8b554
masa <masa.shimomatsu@gmail.com>
2014-03-30 15:37:47 +0900

    Make it simple

--- bonsai/src/aruite/tanoshiku/bousaikunren/MainActivity.java
+++ bonsai/src/aruite/tanoshiku/bousaikunren/MainActivity.java
@@ -28,62 +28,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
-        // Showing status
-        if(status!=ConnectionResult.SUCCESS){ // Google Play Services are not available
+        // Getting reference to the SupportMapFragment of activity_main.xml
+        MapFragment fm = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
 
-            int requestCode = 10;
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
-            dialog.show();
-
-        }else { // Google Play Services are available
-
-            // Getting reference to the SupportMapFragment of activity_main.xml
-            MapFragment fm = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-
-            // Getting GoogleMap object from the fragment
-            googleMap = fm.getMap();
-            googleMap.setMyLocationEnabled(true);
-            
-         // Getting LocationManager object from System Service LOCATION_SERVICE
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-            // Creating a criteria object to retrieve provider
-            Criteria criteria = new Criteria();
-/*
-            // Getting the name of the best provider
-            String provider = locationManager.getBestProvider(criteria, true);
-
-            // Getting Current Location
-            Location location = locationManager.getLastKnownLocation(provider);
-
-            LocationListener locationListener = new LocationListener() {
-            	
-               public void onLocationChanged(Location location) {
-            	  // redraw the marker when get location update.
-            	  drawMarker(location);
-               }};
-               
-            if(location!=null){
-               //PLACE THE INITIAL MARKER              
-               drawMarker(location);
-            }
-            locationManager.requestLocationUpdates(provider, 20000, 0, (android.location.LocationListener) locationListener);
-  */     
-        }
-    }
-    /*
-    private void drawMarker(Location location){
-    	googleMap.clear();
-    	LatLng currentPosition = new LatLng(location.getLatitude(),
-    	location.getLongitude());
-    	googleMap.addMarker(new MarkerOptions()
-    	.position(currentPosition)
-    	.snippet("Lat:" + location.getLatitude() + "Lng:"+ location.getLongitude())
-    	.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-    	.title("ME"));
-    	}
-    */
+        // Getting GoogleMap object from the fragment
+        googleMap = fm.getMap();
+        googleMap.setMyLocationEnabled(true);
+   }
 }
