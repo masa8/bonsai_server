a9ed204c40bfbc028db4bc6b228464e9b462c934
masa <masa.shimomatsu@gmail.com>
2014-04-10 23:01:30 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BonsaiServer::Application.config.secret_key_base = '7178ccdaaac1fa01150ca8d27ab309c2e46eef36fdcca4998a33a34c9be194d681472dcfbcafd17c4f58391fa582a2c89d00f2ddb6708b11185e52a6db1a849f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BonsaiServer::Application.config.session_store :cookie_store, key: '_bonsai_server_session'
